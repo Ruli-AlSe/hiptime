4f24879723247f4e317fa84c6f1d75fa0640abda
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2019-10-07 17:03:43 -0500

    Add logic for completed items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,6 +49,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end
 
 private
 
